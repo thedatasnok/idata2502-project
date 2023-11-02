ed23283777deef687ee777526c1374b5d11659d5
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-11-02 23:37:51 +0100

    feat: provision linodes with a public and a internal network

--- infrastructure/definition/main.tf
+++ infrastructure/definition/main.tf
@@ -1,40 +1,57 @@
-variable "token" {
-  type = string
-}
-
-variable "ssh_public_key" {
-  type = string
+locals {
+  default_token   = file("./token")
+  default_ssh_key = file("~/.ssh/id_rsa.pub")
 }
 
 terraform {
   required_providers {
     linode = {
       source  = "linode/linode"
-      version = "2.8.0"
+      version = "2.9.4"
     }
   }
 }
 
 provider "linode" {
-  token = var.token
+  token = local.default_token
 }
 
 resource "linode_instance" "k3s_server" {
-  count           = 2
-  label           = "k8s-master"
+  count           = 3
+  label           = "k3s-server-${count.index + 1}"
   image           = "linode/ubuntu22.04"
   region          = "se-sto"
   type            = "g6-standard-1"
-  authorized_keys = [var.ssh_public_key]
+  authorized_keys = [local.default_ssh_key]
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose      = "vlan"
+    label        = "internal-network"
+    ipam_address = "192.168.0.${count.index + 2}/24"
+  }
 }
 
 resource "linode_instance" "k3s_agent" {
   count           = 3
-  label           = "k8s-worker"
+  label           = "k3s-agent-${count.index + 1}"
   image           = "linode/ubuntu22.04"
   region          = "se-sto"
   type            = "g6-standard-1"
-  authorized_keys = [var.ssh_public_key]
+  authorized_keys = [local.default_ssh_key]
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose      = "vlan"
+    label        = "internal-network"
+    ipam_address = "192.168.0.${count.index + 10}/24"
+  }
 }
 
 # We need to allow SSH access to the instances to allow Ansible to provision the cluster
@@ -53,5 +70,5 @@ resource "linode_firewall" "node-ingress" {
   inbound_policy  = "DROP"
   outbound_policy = "ACCEPT"
 
-  linodes = [linode_instance.k8s_master.*.id, linode_instance.k8s_worker.*.id]
+  linodes = concat(linode_instance.k3s_server[*].id, linode_instance.k3s_agent[*].id)
 }
